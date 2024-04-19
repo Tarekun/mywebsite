a44468ee35f1e8250abecc001477ef60a14c0d00
Daniele Tarek Iaisy <daniele.tarek.iaisy@hotmail.it>
2024-04-19 15:07:50 +0200

    configured library to set the gradient

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { PageContainer } from "optimus-bo-ui";
 
 export default function Home() {
   return (
     <PageContainer>
       <Typography>What up daaaawg</Typography>
+      <Button variant="outlined">prova</Button>
     </PageContainer>
   );
 }
