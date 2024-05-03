c105e1fcbd3969091fb542b54c1da6b49d910c97
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2024-05-03 21:16:47 +0200

    implement ctr effect on images and improved hobbies layout

--- src/core/sections/Hobbies.tsx
+++ src/core/sections/Hobbies.tsx
@@ -1,25 +1,26 @@
-import { Box, Pagination, Typography } from "@mui/material";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Box, Grid, Pagination, Typography } from "@mui/material";
 import { useLanguagePack } from "optimus-bo-ui/dist/contexts/LanguagePackContext";
 import { useRef, useState } from "react";
 import { Swiper as SwiperClass } from "swiper";
 import "swiper/css";
 import { Controller, Pagination as SwiperPagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "../../App.css";
+import { mainGreenRGB } from "../colors";
 import { LanguagePackSchema } from "../languagePacks";
 
 const hobbies = [
-  {
-    name: "Moda",
-    description:
-      "Sono un grande appassionato di moda. Guardo sfilate e leggo articoli a riguardo da quando avevo 16 anni. Ho iniziato ad interessarmene attraverso la musica; intorno al 2016 un sacco di rapper si erano avvicinati al mondo dello streetwear e dell'alta moda e così ne venni esposto per la prima volta. Mi viene chiesto spesso, ma io tendo a non avere tanto dei marchi preferiti, quanto degli stilisti preferiti tra cui Demna Gvasalia, Martin Margiela e Raf Simons",
-    img: "https://crfashionbook.com/wp-content/uploads/2022/03/martin-margiela-1602001955.jpg",
-  },
   {
     name: "Matematica",
     description:
       "La matematica (e la logica) è diventata la mia scienza preferita in assoluto e devo dire che un po' mi pento di aver studiato informatica invece. Studio matematica nel tempo libero da tempo e direi che l'argomento che ho approfondito di più in assoluto è stata la Teoria dei Tipi. Per poter ampliare l'argomento ancora di più, il mio prossimo obbiettivo è di studiare un po' di Teoria delle Categorie e Teoria delle Omotopie per poter poi comprendere per bene Homotopy Type Theory. Ho anche in programma lo studio completo dell'Analisi Reale perché un giorno vorrei poter studiare come si deve Quantum Field Theory, ma qui sto procrastinando da anni (devo iniziare a fare Teoria della Misura, Analisi 2)",
     img: "https://www.researchgate.net/publication/344704718/figure/fig1/AS:947446209327104@1602900187484/Martin-Loef-type-theory.png",
+  },
+  {
+    name: "Moda",
+    description:
+      "Sono un grande appassionato di moda. Guardo sfilate e leggo articoli a riguardo da quando avevo 16 anni. Ho iniziato ad interessarmene attraverso la musica; intorno al 2016 un sacco di rapper si erano avvicinati al mondo dello streetwear e dell'alta moda e così ne venni esposto per la prima volta. Mi viene chiesto spesso, ma io tendo a non avere tanto dei marchi preferiti, quanto degli stilisti preferiti tra cui Demna Gvasalia, Martin Margiela e Raf Simons",
+    img: "https://crfashionbook.com/wp-content/uploads/2022/03/martin-margiela-1602001955.jpg",
   },
   {
     name: "Musica",
@@ -54,36 +55,43 @@ function HobbyBox({ hobbyIdx }: HobbyBoxProps) {
   const hobby = hobbies[hobbyIdx];
   return (
     <Box sx={{ width: "100%", flexShrink: 0 }}>
-      {hobby.name}
-      <Grid2 container>
-        <Grid2 xs={12} md={6}>
+      <Grid container alignContent="center" alignItems="center">
+        <Grid xs={12} md={5} padding={2}>
           <Box
-            component="img"
-            src={hobby.img}
+            className="crt"
             sx={{
-              width: "100%",
-              height: "auto",
-              filter: "contrast(90%) saturate(80%) blur(0.5px)", // Optional: Adjust image for retro look
-              "::after": {
-                // Use pseudo-element for additional effects like scanlines
-                content: '""',
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
-                backgroundImage:
-                  "linear-gradient(rgba(0,0,0,0) 50%, rgba(0,0,0,0.05) 50%)",
-                backgroundSize: "auto 2px", // Creates fine horizontal lines
-                pointerEvents: "none", // Ensures the pseudo-element does not interfere with interaction
-              },
+              position: "relative",
+              borderRadius: 3,
+              height: "100% ",
             }}
-          />
-        </Grid2>
-        <Grid2 xs={12} md={6}>
+          >
+            <Box
+              className="crt"
+              component="img"
+              src={hobby.img}
+              sx={{
+                width: "100%",
+                maxHeight: "100%",
+                borderRadius: 3,
+                boxShadow: `0px 0px 15px ${mainGreenRGB}`,
+                objectFit: "cover",
+              }}
+            />
+          </Box>
+        </Grid>
+
+        <Grid xs={12} md={7} padding={2}>
+          <Typography
+            textAlign="right"
+            variant="h4"
+            color="primary.main"
+            marginBottom={1}
+          >
+            {hobby.name}
+          </Typography>
           <Typography textAlign="right">{hobby.description}</Typography>
-        </Grid2>
-      </Grid2>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
@@ -119,11 +127,13 @@ export default function Hobbies() {
         variant="h4"
         color="primary.main"
         sx={{ textAlign: "left" }}
-        margin={2}
+        marginBottom={2}
       >
         {title}
       </Typography>
-      <Typography>{introduction}</Typography>
+      <Typography textAlign="left" marginBottom={3}>
+        {introduction}
+      </Typography>
 
       <Swiper
         onSwiper={(swiper: SwiperClass) => (swiperRef.current = swiper)}
@@ -137,21 +147,36 @@ export default function Hobbies() {
       >
         {hobbies.map((_, idx) => {
           return (
-            <SwiperSlide key={idx}>
+            <SwiperSlide
+              key={idx}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                alignContent: "center",
+                height: "100%",
+              }}
+            >
               <HobbyBox hobbyIdx={idx} />
             </SwiperSlide>
           );
         })}
       </Swiper>
 
-      <Pagination
-        page={activeIdx + 1}
-        onChange={(_, value) => switchSlide(value - 1)}
-        count={hobbies.length}
-        variant="outlined"
-        shape="rounded"
-        color="primary"
-      />
+      <Box
+        sx={{ width: "100%" }}
+        display="flex"
+        justifyContent="center"
+        marginTop={2}
+      >
+        <Pagination
+          page={activeIdx + 1}
+          onChange={(_, value) => switchSlide(value - 1)}
+          count={hobbies.length}
+          variant="outlined"
+          shape="rounded"
+          color="primary"
+        />
+      </Box>
     </Box>
   );
 }
